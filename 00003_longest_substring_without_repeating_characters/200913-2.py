5fb20fba3b9ae460f70456ce63cdacd8c83349b2
chris.yang <chris.yang@mindertech.co.uk>
2020-09-13 20:51:36 +0800

    update 无重复字符的最长子串

---
+++ 00003_longest_substring_without_repeating_characters/200913-2.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j-i+1)
+            st[s[j]] = j + 1
+        
+        return ans
+# @lc code=end
+
